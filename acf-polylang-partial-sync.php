a2ed2e0a6ab80b1db6cb6f9225e2a972c7391d6a
zmej-gorunuch <z.g-web@ukr.net>
2020-11-12 15:22:44 +0200

    Update acf-polylang-partial-sync.php
    Added translation for plugin

--- acf-polylang-partial-sync.php
+++ acf-polylang-partial-sync.php
@@ -6,13 +6,19 @@
  * Text Domain: acf-polylang-partial-sync
  */
 
+function acf_polylang_partial_sync_translation_init(){
+	load_plugin_textdomain('acf-polylang-partial-sync', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+}
+
+add_action('plugins_loaded', 'acf_polylang_partial_sync_translation_init');
+
 if(!function_exists('partial_sync_init')):
 function partial_sync_init(){
     /* Adds a field setting to choose whether to synchronize the field or not */
     $add_field_settings = function ($field) {
         acf_render_field_setting( $field, array(
-            'label'         => __('Synchronize Field'),
-            'instructions'  => __('Synchronizes the field between different languages when using Polylang'),
+            'label'         => __('Synchronize Field', 'acf-polylang-partial-sync'),
+            'instructions'  => __('Synchronizes the field between different languages when using Polylang', 'acf-polylang-partial-sync'),
             'name'          => 'sync_field',
             'type'          => 'true_false',
             'ui'            => 1,
